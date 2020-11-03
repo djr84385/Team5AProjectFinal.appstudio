8d6b6efba42bd622af92ea1aa6c4e1df9bbcb966
Jrp85607 <54645651+Jrp85607@users.noreply.github.com>
2020-11-03 01:51:57 -0600

    Night

--- forms/userBucketlist/userBucketlist.js
+++ forms/userBucketlist/userBucketlist.js
@@ -6,21 +6,6 @@ userName = "jrp85607"
 database = "375groupa5"
 let currentUserEvents = []
 
-console.log(accountName)
-
-query = "SELECT event_name, description, date_added, date_completed FROM `events` e INNER JOIN bucketlists b ON e.bucket_id = b.bucket_id INNER JOIN `user` u ON u.user_id = b.user_id WHERE u.username = '" + accountName + "';"
-console.log(query)
-
-req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + userName + "&pass=" + pw + "&database=" + database + "&query=" + query)
-
-if (req.status == 200) { //transit worked.
-  currentUserEvents = JSON.parse(req.responseText)
-  console.log(currentUserEvents)
-} else
-  console.log("error")
-  
-let dataJson = JSON.stringify(currentUserEvents);
-
 // class values are here:
 // http://getbootstrap.com/css/#type-alignment
 let columns1 = [
@@ -30,24 +15,27 @@ let columns1 = [
             {title: "Date Completed"},
         ];
 
-        
 function Main() {
   updateTable();
-}
+  }
+
+function updateTable(Events) {
+  DataTable1.settings.columns = columns1;
+  DataTable1.settings.data = Events;
+  DataTable1.build();
+  }
+
 
 DataTable1.onclick = function(event) {
   if(typeof(event.target._DT_CellIndex) != "object" ) { return; }
- var row,col;
+  var row,col;
   row = event.target._DT_CellIndex.row;
   col = event.target._DT_CellIndex.column;
   NSB.MsgBox("Click on "  +  row  +  ", "  +  col  +  ". Value is '"  +  currentUserEvents[row][col]  +  "'.");
 };
 
-function updateTable() {
-  DataTable1.settings.columns = columns1;
-  DataTable1.settings.data = currentUserEvents;
-  DataTable1.build();
-}
+
+
 
 Button1.onclick = function() {
   currentUserEvents[0][0] += "+"; //Just to make a change to the table
@@ -98,5 +86,33 @@ Fliptoggle5.onchange = function() {
   DataTable1.settings.searching = Fliptoggle5.value;
   updateTable();
 };
+
+
+userBucketlist.onshow=function(){
+  
+  console.log(accountName)
+
+  query = "SELECT event_name, description, date_added, date_completed FROM `events` e INNER JOIN bucketlists b ON e.bucket_id = b.bucket_id INNER JOIN `user` u ON u.user_id = b.user_id WHERE u.username = '" + accountName +  "';"
+  console.log(query)
+
+  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + userName + "&pass=" + pw + "&database=" + database + "&query=" + query)
+
+  if (req.status == 200) { //transit worked.
+    currentUserEvents = JSON.parse(req.responseText)
+    console.log(currentUserEvents)
+  } else
+    console.log("error")
+
+  dataJson = JSON.stringify(currentUserEvents);
+  console.log(dataJson)
+  
+  updateTable(currentUserEvents)
+}
+
+
+
+
+
+
 
 
